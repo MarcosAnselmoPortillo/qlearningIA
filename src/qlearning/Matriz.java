6b8c44df874d4881e8a8c7a373f4d9afc6469075
marcos <marcos@marcos-PC>
2013-07-08 16:47:46 -0300

    limpieza de codigo

--- src/qlearning/Matriz.java
+++ src/qlearning/Matriz.java
@@ -125,7 +125,7 @@ public class Matriz {
                 estados[i].acciones.add(a);
             }              
         }
-         aprendizaje.start();
+        aprendizaje.start(); 
      }
   
     /**
